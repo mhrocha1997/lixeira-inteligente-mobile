e8a1e7ea0b5f4d5108d52ebd8f132d01c0e5df15
mhrocha1997 <marcelo.h.rocha1997@gmail.com>
2021-01-31 18:15:32 -0300

    [WIP] Tela de login

---
+++ src/routes.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+
+const AppStack = createStackNavigator();
+
+import Login from './pages/login';
+
+export default function Routes(){
+    return(
+        <NavigationContainer>
+            <AppStack.Navigator screenOptions={{headerShown: false}}>
+                <AppStack.Screen name ="Login" component = {Login} />
+            </AppStack.Navigator>
+        </NavigationContainer>
+    );
+}
