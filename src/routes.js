db1d5324c9cd8c0f3234cf0b1dc8f5e59926c0bd
Marcelo <marcelo.h.rocha1997@gmail.com>
2021-03-30 20:11:31 -0300

    [WIP] Setando token e login com context

--- src/routes.js
+++ src/routes.js
@@ -1,114 +1,99 @@
-import React, {useState} from 'react';
-import { NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
-
-import 'react-native-gesture-handler';
-
+import React, {useContext} from 'react';
 import {StyleSheet, Text} from 'react-native';
 
-import {createDrawerNavigator} from '@react-navigation/drawer';
-import Icon from 'react-native-vector-icons/Ionicons';
 
-import Catalog from './pages/Catalog';
-import MyDiscards from './pages/MyDiscards';
-import ReadBarcode from './pages/ReadBarcode';
+import { NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {createDrawerNavigator} from '@react-navigation/drawer';
+
 import AsyncStorage from '@react-native-community/async-storage'
+
+import Icon from 'react-native-vector-icons/Ionicons';
 
 
 import Login from './pages/Login';
 import Register from './pages/Register';
 
-export default function Routes(){
-    const [isSignedIn,setIsSignedIn] = useState(true);
+import Catalog from './pages/Catalog';
+import MyDiscards from './pages/MyDiscards';
+import ReadBarcode from './pages/ReadBarcode';
 
+import UserContext, { UserProvider } from './contexts/UserContext';
+
+export default function Routes(){
 
     const AppStack = createStackNavigator();
     const Drawer = createDrawerNavigator();
 
-    useEffect( () => {
+    const {isSigned} = useContext(UserContext);
 
-        async function verifyToken(){
-            try {
-                console.log("lendo token...");
-                const token =  AsyncStorage.getItem('token');
-                if(token !== null) {
-                  const response = await api.get('/token', {'headers': {"Authorization": token}});
-      
-                  if(response.status != 200) navigation.navigate("Login");
-      
-                }   
-              } catch(e) {
-                console.log("Erro ao ler o token");
-              }
-        }
-        
-        verifyToken();
-      },[])
-
-      
     return(
-        <NavigationContainer>
-            { isSignedIn 
-                ?( <Drawer.Navigator 
-                    initialRouteName="Meus descartes" 
-                    drawerContentOptions={{
-                        activeTintColor: 'white', 
-                        contentContainerStyle: styles.drawer,
-                        }}
-                    hideStatusBar={true}
-                    screenOptions={{
-                        headerShown: true,
-                        headerLeft: () => null,
-                        headerRight: () => 
-                        <Text style={styles.header}> 600 xp</Text>,
-                        headerTitle: () => null,
-                        headerStyle: styles.headerContainer
-                        
-                    }}
-                    >
-                        <Drawer.Screen 
-                        name="Catálogo" 
-                        component={Catalog}
-                        options={{
-                            // drawerLabel: () => null,
-                            drawerIcon: () => 
-                            <Icon 
-                                name='list' 
-                                size={30}
-                            />,
-                            }}
-                        />
 
-                        <Drawer.Screen 
-                        name="Meus descartes" 
-                        component={MyDiscards}
-                        options={{
-                            // drawerLabel: () => null,
-                            drawerIcon: () =>
-                            <Icon 
-                                name='trash-outline' 
-                                size={30}
-                            />,
+        <UserProvider>
+            <NavigationContainer>
+                { isSigned
+                    ?( <Drawer.Navigator 
+                        initialRouteName="Meus descartes" 
+                        drawerContentOptions={{
+                            activeTintColor: 'white', 
+                            contentContainerStyle: styles.drawer,
                             }}
-                        />
-                        <Drawer.Screen 
-                        name="Novo Descarte" 
-                        component={ReadBarcode}
-                        options={{
-                            drawerLabel: () => null,
-                            title: undefined,
-                            drawerIcon: () => null
-                            }}
-                        />
-                    </Drawer.Navigator>
-                ) : (
-                    <AppStack.Navigator screenOptions={{headerShown: false}}>
-                        <AppStack.Screen name ="Login" component = {Login} />
-                        <AppStack.Screen name ="Register" component = {Register} />
-                    </AppStack.Navigator>
-                )
-            }
-        </NavigationContainer>
+                        hideStatusBar={true}
+                        screenOptions={{
+                            headerShown: true,
+                            headerLeft: () => null,
+                            headerRight: () => 
+                            <Text style={styles.header}> 600 xp</Text>,
+                            headerTitle: () => null,
+                            headerStyle: styles.headerContainer
+                            
+                        }}
+                        >
+                            <Drawer.Screen 
+                            name="Catálogo" 
+                            component={Catalog}
+                            options={{
+                                // drawerLabel: () => null,
+                                drawerIcon: () => 
+                                <Icon 
+                                    name='list' 
+                                    size={30}
+                                />,
+                                }}
+                            />
+
+                            <Drawer.Screen 
+                            name="Meus descartes" 
+                            component={MyDiscards}
+                            options={{
+                                // drawerLabel: () => null,
+                                drawerIcon: () =>
+                                <Icon 
+                                    name='trash-outline' 
+                                    size={30}
+                                />,
+                                }}
+                            />
+                            <Drawer.Screen 
+                            name="Novo Descarte" 
+                            component={ReadBarcode}
+                            options={{
+                                drawerLabel: () => null,
+                                title: undefined,
+                                drawerIcon: () => null
+                                }}
+                            />
+                        </Drawer.Navigator>
+                    ) : (
+                        <AppStack.Navigator screenOptions={{headerShown: false}}>
+                            <AppStack.Screen name ="Login" component = {Login} />
+                            <AppStack.Screen name ="Register" component = {Register} />
+                        </AppStack.Navigator>
+                    )
+                }
+            </NavigationContainer>
+        </UserProvider>
+        
     );
 }
 
