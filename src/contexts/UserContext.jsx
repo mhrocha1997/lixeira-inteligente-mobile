ab9c807a7ebe929e87d908899f79baa82c71f7f5
Marcelo <marcelo.h.rocha1997@gmail.com>
2021-04-01 18:35:09 -0300

    [ADD] Usando dispatch

--- src/contexts/UserContext.jsx
+++ src/contexts/UserContext.jsx
@@ -1,27 +1,59 @@
 import React, {createContext, useEffect, useState, useReducer} from 'react';
+import AsyncStorage from '@react-native-community/async-storage'
+
+import api from '../services/api';
 
 const UserContext = createContext({})
-
-const actions = {
-    login(state,action){
-        setToken(action.token);
-        setIsSigned(true);
-        return {
-            ...state,
-            token,
-            isSigned
-        }
-    }
-}
 
 export const UserProvider = ({children}) =>{
     const [token, setToken] = useState();
     const [isSigned, setIsSigned] = useState(false);
 
+    useEffect( () => {
+
+        async function verifyToken(){
+            try {
+                console.log("lendo token...");
+                const token =  await AsyncStorage.getItem('token');
+                console.log("Token: ", token)
+                if(token !== null) {
+                    setToken(token);
+                    const response = await api.get('/token', {'headers': {"Authorization": token}});
+                    if(response.status==200){
+                        console.log("É verdade")
+                        setIsSigned(true)
+                        
+                    }else{
+                        setIsSigned(false)
+                    }
+        
+                }   
+              } catch(e) {
+                console.log("Erro ao ler o token", e);
+              }
+        }
+        
+        verifyToken();
+      },[])
+
+    const actions = {
+        login(state,action){
+            console.log("state: ",state);
+            setToken(action.payload);
+            setIsSigned(true);
+            return {
+                ...state,
+                token,
+                isSigned
+            }
+        }
+    }
+
     const initialState = {
         token,
         isSigned
     }
+
 
     const [state, dispatch] = useReducer(reducer, initialState)
 
@@ -31,31 +63,7 @@ export const UserProvider = ({children}) =>{
     }
 
 
-    useEffect( () => {
-
-        async function verifyToken(){
-            try {
-                console.log("lendo token...");
-                const token =  AsyncStorage.getItem('token');
-                if(token !== null) {
-                    setToken(token);
-
-                    const response = await api.get('/token', {'headers': {"Authorization": token}});
-                    
-                    if(response.status==200){
-                        setIsSigned(true)
-                    }else{
-                        setIsSigned(false)
-                    }
-        
-                }   
-              } catch(e) {
-                console.log("Erro ao ler o token");
-              }
-        }
-        
-        verifyToken();
-      },[])
+    
 
     return(
         <UserContext.Provider
