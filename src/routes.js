a4cf244ded56c2b8162c9680ee4afd046a35e27d
Marcelo <marcelo.h.rocha1997@gmail.com>
2021-04-02 16:07:40 -0300

    [ADD] Migraçao para Context API

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,12 @@
 import React, {useContext} from 'react';
-import {StyleSheet, Text} from 'react-native';
+import {StyleSheet, Text, Dimensions} from 'react-native';
 
 
 import { NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import {createDrawerNavigator} from '@react-navigation/drawer';
 
-import AsyncStorage from '@react-native-community/async-storage'
-
 import Icon from 'react-native-vector-icons/Ionicons';
-
 
 import Login from './pages/Login';
 import Register from './pages/Register';
@@ -18,19 +15,22 @@ import Catalog from './pages/Catalog';
 import MyDiscards from './pages/MyDiscards';
 import ReadBarcode from './pages/ReadBarcode';
 
-import UserContext, { UserProvider } from './contexts/UserContext';
+import UserContext from './contexts/UserContext';
+
+const width = Dimensions.get("screen").width;
+
 
 export default function Routes(){
 
     const AppStack = createStackNavigator();
     const Drawer = createDrawerNavigator();
 
-    const {isSigned} = useContext(UserContext);
-    console.log("Está logado:", isSigned);
+    const {isSigned, token} = useContext(UserContext);
     
-    return(
+    console.log("Está logado:", isSigned);
 
-        <UserProvider>
+
+    return(
             <NavigationContainer>
                 { isSigned
                     ?( <Drawer.Navigator 
@@ -47,7 +47,10 @@ export default function Routes(){
                             <Text style={styles.header}> 600 xp</Text>,
                             headerTitle: () => null,
                             headerStyle: styles.headerContainer
-                            
+                        
+                        }}
+                        drawerStyle={{
+                            width: width*0.65
                         }}
                         >
                             <Drawer.Screen 
@@ -93,8 +96,6 @@ export default function Routes(){
                     )
                 }
             </NavigationContainer>
-        </UserProvider>
-        
     );
 }
 
@@ -102,7 +103,7 @@ const styles = StyleSheet.create({
     drawer:{
       // alignItems: 'center',
       backgroundColor: '#31ce8c',
-      justifyContent: 'center',
+    //   justifyContent: 'center',
       flex: 1,
     },
     header: {
