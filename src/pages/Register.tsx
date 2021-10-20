9f4c9ee09b489ee64e3c0db999f35ecd9719316c
Marcelo <marcelo.h.rocha1997@gmail.com>
2021-10-20 20:04:50 -0300

    [WIP] Register Form Validation

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,15 +1,18 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {
     StyleSheet,
     Text,
     TouchableOpacity,
     View,
 } from 'react-native';
+import {useForm} from 'react-hook-form';
+import * as yup from 'yup';
+import {yupResolver} from '@hookform/resolvers/yup';
 
 import Logo from '../components/Logo';
 import Background from '../components/Background';
 import Header from '../components/header';
-import TextInput from '../components/TextInput';
+import TextField from '../components/TextField';
 import Button from '../components/Button';
 
 import colors from '../styles/colors';
@@ -18,25 +21,59 @@ import fonts from '../styles/fonts';
 import {signup} from '../services/UserService';
 
 export default function Register({navigation} : any){
-    const[nome,setName] = useState({value: '', error: ''});
-    const [email,setEmail] = useState({value: '', error: ''});
-    const [ password, setPassword] = useState({value: '', error: ''});
+    const [fieldErrors, setFieldErrors] = useState({
+        'name': '',
+        'email': '',
+        'password': '',
+        'passwordConfirmation': '',
+    })
 
+    const fieldsValidationSchema = yup.object().shape({
+        name: yup
+        .string()
+        .required('O nome não pode ser vazio'),
+        email: yup
+        .string()
+        .required('O email não pode ser vazio')
+        .email('Digite um email válido'),
+        password: yup
+        .string()
+        .required('A senha não pode ser vazia')
+        .min(6, 'A senha deve conter pelo menos 6 dígitos'),
+        passwordConfirmation: yup
+        .string()
+        .oneOf([yup.ref('password'), null], 'Confirme a sua senha corretamente!')
+      })
 
-    async function onSignUpPressed(e: any){
-        // ### TODO: Fazer validação dos campos
-        
-        e.preventDefault();
+    const {register, setValue, handleSubmit, formState: {errors}, clearErrors} = useForm(
+        {resolver: yupResolver(fieldsValidationSchema)});
 
-        const data = {
-            name: nome.value,
-            email: email.value,
-            password: password.value,
-            passwordConfirmation: password.value
+    useEffect(()=>{
+        register('name');
+        register('email');
+        register('password');
+        register('passwordConfirmation');
+    },[register])
+
+    useEffect(()=>{
+        let auxErrors = fieldErrors;
+        Object.keys(errors).forEach((error)=>{
+           auxErrors[error] = errors[error].message;
+        })
+        setFieldErrors(auxErrors);
+        console.log(fieldErrors);
+    },[errors])
+
+    async function onSignUpPressed(data: any){
+        const body = {
+            name: data.name,
+            email: data.email,
+            password: data.password,
+            passwordConfirmation: data.passwordConfirmation
         };
 
         try{
-            const signupConfirm = await signup(data);
+            const signupConfirm = await signup(body);
             console.log(signupConfirm)
             signupConfirm ?
                 navigation.reset({
@@ -63,39 +100,38 @@ export default function Register({navigation} : any){
             <Logo />
             <Header> Cadastre-se e ganhe recompensas pelo seu lixo!</Header>
 
-            <TextInput
-                label="Nome"
+            <TextField
+                placeholder="Nome"
                 returnKeyType="next"
-                value={nome.value}
-                onChangeText={(text: any) => setName({ value: text, error: '' })}
-                error={!!nome.error}
-                errorText={nome.error}
-                description="Teste"
+                onChangeText={(text: string) => setValue('name', text)}
+                error={fieldErrors.name}
             />
             
-            <TextInput 
-                label="Email"
+            <TextField 
+                placeholder="Email"
                 returnKeyType="next"
-                value={email.value}
-                onChangeText={(text: any) => setEmail({value: text, error:''})}
-                error={!!email.error}
-                errorText={email.error}
-                description="Teste"
+                onChangeText={(text: string) => setValue('email', text)}
+                error={fieldErrors.email}
             />
-            <TextInput
-                label="Senha"
-                returnKeyType="done"
-                value={password.value}
-                onChangeText={(text: any) => setPassword({ value: text, error: '' })}
-                error={!!password.error}
-                errorText={password.error}
+            <TextField
+                placeholder="Senha"
+                returnKeyType="next"
+                onChangeText={(text: string) => setValue('password', text)}
                 secureTextEntry
-                description="Teste"
+                error={fieldErrors.password}
             />
 
+            <TextField
+                placeholder="Confirme sua senha"
+                returnKeyType="done"
+                onChangeText={(text: string) => setValue('passwordConfirmation', text)}
+                secureTextEntry
+                error={fieldErrors.passwordConfirmation}
+            />
+            
             <Button 
                 mode="contained"
-                onPress={onSignUpPressed}
+                onPress={handleSubmit(onSignUpPressed)}
                 style={{marginTop: 24}}
             >
                 Enviar
@@ -126,5 +162,5 @@ const styles = StyleSheet.create({
     login: {
         flexDirection: 'row',
         fontFamily: fonts.text,
-    }
+    },
   })
